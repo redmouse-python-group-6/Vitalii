1342fdc817eda9c29e439ca3d6a1d47992564056
pehas <pehas.90@gmail.com>
2017-04-25 20:44:26 +0600

    Create txt and script

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-print(1+1)
+f=open('tmp.txt')
+# for l in f:
+# 	print(l)
+
+print(f.read(2))
+print(f.tell())
+f.seek(0, 0)
+print(f.tell())
+print(f.read())
+f.close()
+
+
+f=open('tmp.txt', 'w')
+for i in [str(x) for x in range(20)]:
+	f.writelines(["%s-%s"%(i, int(i)**2), "%s-%s"%(i, int(i)**2)])
+f.close()
