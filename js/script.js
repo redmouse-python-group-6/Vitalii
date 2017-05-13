7763cfb8199db6b7a9d011f4ad40848a261d0a33
pehas <pehas.90@gmail.com>
2017-05-13 20:55:30 +0600

    add form in page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $('.img_class').click(function(){
    $("#head_div").animate({width:280}, 2000);
    $.ajax({
    	url: "google.kg/feed"
+   	method: "POST"
    	success: function(msg){
    		$('#head_div').text=msg['content']
    	}
